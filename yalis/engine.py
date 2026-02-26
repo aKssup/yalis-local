a469f2040a82db322290d1a920a616c97a6a1054
aKssup <118090347+aKssup@users.noreply.github.com>
2026-02-25 23:01:31 -0500

    mark_dynamic

--- yalis/engine.py
+++ yalis/engine.py
@@ -429,12 +429,12 @@ class LLMEngine:
                     slot_ids = self.kv_slots_manager.allocate(req_ids, lens)
                     block_table = self.kv_slots_manager.view(slot_ids)
 
-                    # dynamo.mark_dynamic(token_counter, 0, min=1, max=self.inference_config.max_batch_size) # B
-                    # dynamo.mark_dynamic(block_table, 0, min=1, max=self.inference_config.max_batch_size) # B
+                    dynamo.mark_dynamic(token_counter, 0, min=1, max=self.inference_config.max_batch_size) # B
+                    dynamo.mark_dynamic(block_table, 0, min=1, max=self.inference_config.max_batch_size) # B
 
-                # dynamo.mark_dynamic(tokens, 0, min=1, max=self.inference_config.max_batch_size) # B
-                # dynamo.mark_dynamic(tokens, 1, min=1, max=1024) # T
-                # dynamo.mark_dynamic(lens, 0 , min=1, max=self.inference_config.max_batch_size) # B
+                dynamo.mark_dynamic(tokens, 0, min=1, max=self.inference_config.max_batch_size) # B
+                dynamo.mark_dynamic(tokens, 1, min=1, max=1024) # T
+                dynamo.mark_dynamic(lens, 0 , min=1, max=self.inference_config.max_batch_size) # B
 
                 _ = prefill(
                     model=self.model, 
